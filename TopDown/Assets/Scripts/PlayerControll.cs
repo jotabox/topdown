c68332aac099e09bea49507c601e7a5351a3cb7e
jotabox <jotapaulo.box@gmail.com>
2023-07-03 09:52:58 -0300

    primeiro commit , movimentação
    feita a movimentação em 8 direçoes do personagem

---
+++ TopDown/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    private Rigidbody2D rigi;
+    private float speedMoveX;
+    private float speedMoveY;
+    [SerializeField] private float moviment;
+
+
+    private void Awake()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    void Move()
+    {
+        // pegando a direção do movimento
+        speedMoveX = Input.GetAxisRaw("Horizontal");
+        speedMoveY = Input.GetAxisRaw("Vertical");
+
+        // recuperando a direção e fazendo o calculo para as diagonais
+        Vector2 direction = new Vector2(speedMoveX, speedMoveY).normalized;
+
+        rigi.velocity = direction * moviment;
+
+
+
+
+    }
+}
